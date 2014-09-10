919e325cc2376610d49d9ea8e725d14673bd172d
Stephen <steveyoung359@gmail.com>
2014-09-10 15:41:17 -0500

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+/* jshint globalstrict:true */
+
+'use strict';
+
+// gulp plugins
+var gulp       = require('gulp'),
+  scsslint     = require('gulp-scss-lint')
+
+var exitOnError = !!gulp.env['exit-on-error'];
+
+gulp.task('lint-scss', function() {
+  gulp.src('./css/**/*.scss')
+    .pipe(scsslint())
+
+});
+
